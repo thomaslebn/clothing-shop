aa55689b7999331010ecd50a57f2076f4bafb41c
thomaslebn <lebn.thomas@pm.me>
2021-04-02 11:19:15 +0200

    Updating a little tweak in cart-dropdown

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -13,27 +13,30 @@ import CartItem from "../cart-item/cart-item";
 
 import "./cart-dropdown.scss";
 
-const CartDropdown = ({ cartItems, history, dispatch }) => (
-  <div className="cart-dropdown">
-    <div className="cart-items">
-      {cartItems.length ? (
-        cartItems.map((cartItem) => (
+const CartDropdown = ({ cartItems, history, dispatch }) =>
+  cartItems.length ? (
+    <div className="cart-dropdown">
+      <div className="cart-items">
+        {cartItems.map((cartItem) => (
           <CartItem key={cartItem.id} item={cartItem} />
-        ))
-      ) : (
-        <span className="empty-message">Your cart is empty</span>
-      )}
+        ))}
+      </div>
+      <CustomButton
+        onClick={() => {
+          history.push("/checkout");
+          dispatch(toggleCartHidden());
+        }}
+      >
+        GO TO CHECKOUT
+      </CustomButton>
     </div>
-    <CustomButton
-      onClick={() => {
-        history.push("/checkout");
-        dispatch(toggleCartHidden());
-      }}
-    >
-      GO TO CHECKOUT
-    </CustomButton>
-  </div>
-);
+  ) : (
+    <div className="cart-dropdown empty-cart-dropdown">
+      <div className="cart-items">
+        <span className="empty-message">Your cart is empty</span>
+      </div>
+    </div>
+  );
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
